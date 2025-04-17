845ef456b416013556e7d351da51f5a623dec3b0
ndegz <ndegwa8781@gmail.com>
2025-04-17 13:26:14 +0300

    finished mobile & tab design

--- src/main.js
+++ src/main.js
@@ -5,27 +5,46 @@ const icon = document.querySelector('.theme-icon');
 const themeTitle = document.querySelector('.title-header');
 const darkmode = document.getElementById('body') 
 const headerBar = document.querySelector('.header-bar')
+const navbtns = document.querySelectorAll('.nav-btn')
+const cards = document.querySelectorAll('.card')
+const content = document.querySelectorAll('.content')
+const names = document.querySelectorAll('.name')
+const removeBtns = document.querySelectorAll('.remove-btn')
 
 
 icon.addEventListener('click', () => {
     const items = [themeTitle, darkmode, headerBar]
+    const darkNavItems = [icon, logo]
 
-    icon.classList.toggle('active')
-    logo.classList.toggle('active')
+    darkNavItems.forEach(item => {
+        item.classList.toggle('active');
+    })
+   
+    navbtns.forEach(item => {
+        item.classList.toggle('active')
+    })
+
+    cards.forEach(item => {
+        item.classList.toggle('active')
+    })
     
     items.forEach(item => {
         item.classList.toggle('dark')
     })
+
+    content.forEach(item => {
+        item.classList.toggle('active')
+    })
+
+    names.forEach(item => {
+        item.classList.toggle('active')
+    })
+
+    removeBtns.forEach(item => {
+        item.classList.toggle('active')
+    })
    
 })
-
-
-
-
-
-
-
-
 
 
 //cards toggle button transitions
